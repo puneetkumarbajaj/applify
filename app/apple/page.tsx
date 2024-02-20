f950ac5de80ce316f9ae158b0f6c0f2f90ca6ace
Puneet Bajaj <puneetbajaj591@gmail.com>
2024-02-20 10:24:20 -0500

    Initial sub-project setup for both the UIs

---
+++ app/apple/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import * as React from 'react';
+
+export interface IAppleMusicProps {
+}
+
+export default class AppleMusic extends React.Component<IAppleMusicProps> {
+  public render() {
+    return (
+      <div>
+        I am Apple Music Page
+      </div>
+    );
+  }
+}
---
+++ app/spotify/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import * as React from 'react';
+
+export interface ISpotifyProps {
+}
+
+export default class Spotify extends React.Component<ISpotifyProps> {
+  public render() {
+    return (
+      <div>
+        I am spotify home page
+      </div>
+    );
+  }
+}
