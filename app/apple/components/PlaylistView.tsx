408fe627f6ef5c65dc5c7a3d40a0f7ec04cf1208
Puneet Bajaj <puneetbajaj591@gmail.com>
2024-03-20 16:04:50 -0400

    pulling track data from musickit v0.04

--- app/apple/components/PlaylistView.tsx
+++ app/apple/components/PlaylistView.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Header } from './header';
 import { IoIosPlay } from "react-icons/io";
 import { getMusicKitInstance } from '@/app/api/musickit';
+import { formatDistanceToNow } from 'date-fns';
 
 export interface IPlaylistViewProps {
     globalPlaylistId: string | null;
@@ -34,17 +35,20 @@ export function PlaylistView (props: IPlaylistViewProps) {
   return (
     <div className= 'h-full w-full flex flex-col m-3 rounded-xl overflow-y-scroll'>
         <div>
-            <Header />
             <div className='flex p-5 gap-7'>
-                <div className='h-[232px] w-[232px] shadow-2xl shadow-black'>
+                <div className='h-[232px] w-[232px]'>
                     <img src={playlistData?.attributes?.artwork?.url} alt="playlist" />
                 </div>
                 <div className='mt-20'>
                     <div className='text-xs'>Playlist</div>
                     <div className='font-bold text-6xl font-sans'>{playlistData?.attributes?.name}</div>
                     <div className='text-xs mt-5 text-neutral-300'>{playlistData?.attributes?.curatorName}</div>
-                    <div className='text-xs font-semibold mt-5'>{playlistData?.attributes?.description?.toString()}</div>
-                    <div className='text-xs font-semibold mt-5'>{playlistData?.attributes?.lastModifiedDate}</div>
+                    <div className='text-xs font-semibold mt-5'>{playlistData?.attributes?.description?.short}</div>
+                    <div className='text-xs font-semibold mt-5'>
+                        {playlistData?.attributes?.lastModifiedDate ?
+                            `${formatDistanceToNow(new Date(playlistData.attributes.lastModifiedDate))} ago` :
+                            'Date not available'}
+                    </div>
                 </div>
             </div>
         </div>
@@ -62,13 +66,16 @@ export function PlaylistView (props: IPlaylistViewProps) {
                     {songData.map((track, index) => (
                         <tr 
                             key={index} 
-                            className='hover:bg-neutral-800' 
+                            className='hover:bg-accent-foreground' 
                             onClick={() => {
                                 props.globalsetCurrentSongId(track.id)
                                 props.globalsetIsPlaying(true)
                             }}
                         >
                             <td className='flex p-2 pt-3'>
+                                <div className='mr-2'> 
+                                    <img src={track.attributes?.artwork?.url} alt="Track" className="w-10 h-10" /> 
+                                </div>
                                 <div className='flex flex-col justify-center'>
                                     {track.attributes?.name}
                                 </div>
@@ -79,8 +86,8 @@ export function PlaylistView (props: IPlaylistViewProps) {
                             </td>
 
                             <td className='pt-2 text-xs pr-5'>
-                                {Math.floor(track.attributes?.durationInMillis ?? 0 / 60000)}:
-                                {((track.attributes?.durationInMillis ?? 0) % 60000 / 1000).toFixed(0).padStart(2, '0')}
+                                {Math.floor((track.attributes?.durationInMillis ?? 0) / 60000)}:
+                                {(((track.attributes?.durationInMillis ?? 0) % 60000) / 1000).toFixed(0).padStart(2, '0')}
                             </td>
                         </tr>
                     ))}
